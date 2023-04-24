9fa78ad7ab082040f338443fe0fd1ef2400a380f
Terre8055 <michaelappiah2018@icloud.com>
2023-04-24 01:04:48 +0000

    Add react icons for small screen

--- vite-project/src/Component/Homepage/Home.jsx
+++ vite-project/src/Component/Homepage/Home.jsx
@@ -1,23 +1,54 @@
 import React from "react";
 import {useNavigate} from 'react-router-dom'
+import {BiHomeSmile as Homeicon} from 'react-icons/bi'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+
 
 import './Home.css'
+
+const Menu =() => (
+  <>
+    <p>Features</p>
+    <p>Promo</p>
+    <p>How-to</p>
+  </>
+)
+
 
 
 export default function Home(){
 
   const navigate = useNavigate()
+  const [toggleMenu, setToggleMenu] = React.useState(false)
+
   return (
     // Navbar
     <main id="home-view">
       <nav className="navbar">
         <div className="navbar-title">codingbee</div>
         <div className="nav-links">
+          {/* <div><Homeicon/></div> */}
           <div>Features</div>
           <div>Promo</div>
           <div>How to</div>
           <button className="learn-more-color">Learn more</button>
         </div>
+        <div className="gpt3__navbar-menu">
+          {toggleMenu
+            ? <RiCloseLine  size={60} onClick={(e) => setToggleMenu(false)}></RiCloseLine>
+            : <RiMenu3Line size={60} onClick={(e) => setToggleMenu(true)}></RiMenu3Line>
+          }
+          {toggleMenu && (
+            <div className="gpt3__navbar-menu_container scale-up-center">
+              <div className="gpt3__navbar-menu_container-links">
+                 <Menu />
+                <button className="learn-more-color">Learn more</button>
+              </div>
+            </div>
+          )
+
+          }
+      </div>
       </nav>
       <section className="content-body">
         <div className="page-intro">
@@ -34,3 +65,4 @@ export default function Home(){
     </main>
   )
 }
+
